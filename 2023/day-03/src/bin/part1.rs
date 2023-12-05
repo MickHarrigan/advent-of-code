6da728a9e0232592f35d3fc8c5f58c0cfa2eb73a
Mick Harrigan <mharrigan328@gmail.com>
2023-12-05 13:20:51 -0800

    that was rough. I need to use regex more and not use 2d arrays like that

---
+++ 2023/day-03/src/bin/part1.rs
@@ -0,0 +1,162 @@
+use anyhow::Result;
+use std::{fs::File, io::Read};
+
+// I feel like a big dumb idiot after this one.
+// this is such a mess I cannot believe that I did this lol
+fn main() -> Result<()> {
+    // read in the inputs of the file to the contents string
+    let mut input = File::open(format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "input1.txt"))?;
+    let mut contents = String::new();
+    input.read_to_string(&mut contents)?;
+    let out = parse_schematic(contents);
+    println!("Output: {}", out);
+    Ok(())
+}
+
+// the contents of the schematic are one of three things,
+// a number,
+// a symbol,
+// or a period (nothing)
+#[derive(Debug)]
+enum Item {
+    Number(usize, Region),
+    Symbol(Location),
+}
+
+// this is the top left and bottom right corners of a rectangle
+#[derive(Default, Clone, Debug)]
+struct Region {
+    // is 2 locations and the connecting area
+    min: Location,
+    max: Location,
+}
+
+impl Region {
+    pub fn contains(&self, loc: &Location) -> bool {
+        (loc.x >= self.min.x && loc.x <= self.max.x) && (loc.y >= self.min.y && loc.y <= self.max.y)
+    }
+}
+
+// this is a specific coordinate (or char (i,j) index)
+#[derive(Default, Clone, Debug)]
+struct Location {
+    x: u32,
+    y: u32,
+}
+
+fn parse_schematic(contents: String) -> usize {
+    // parse each line into a vec of items
+    let things: Vec<Vec<Item>> = contents
+        .lines()
+        .zip(0u32..)
+        .map(|(line, line_num)| {
+            // iterate across each char, match it as an Item
+            let mut num = String::new();
+            let mut region = Region::default();
+            let mut list = Vec::new();
+            for (ind, ch) in line.char_indices() {
+                match ch {
+                    '.' => {
+                        if num.len() != 0 {
+                            let val = num.parse::<usize>().unwrap();
+                            region.max = Location {
+                                x: ind as u32,
+                                y: line_num.saturating_add(1),
+                            };
+                            list.push(Item::Number(val, region.clone()));
+                            num.clear();
+                            region = Region::default();
+                        }
+                    }
+                    '0'..='9' => {
+                        // get the region as well as the number
+                        if num.len() == 0 {
+                            region.min = Location {
+                                x: ind.saturating_sub(1) as u32,
+                                y: line_num.saturating_sub(1),
+                            };
+                        }
+                        num.push(ch);
+                    }
+                    '#' | '%' | '&' | '*' | '+' | '-' | '/' | '=' | '@' | '$' => {
+                        if num.len() != 0 {
+                            let val = num.parse::<usize>().unwrap();
+                            region.max = Location {
+                                x: ind as u32,
+                                y: line_num + 1,
+                            };
+                            list.push(Item::Number(val, region.clone()));
+                            num.clear();
+                            region = Region::default();
+                        }
+                        list.push(Item::Symbol(Location {
+                            x: ind as u32,
+                            y: line_num,
+                        }));
+                    }
+                    _ => unreachable!(),
+                }
+            }
+            if num.len() != 0 {
+                let val = num.parse::<usize>().unwrap();
+                region.max = Location {
+                    x: line.len() as u32,
+                    y: line_num + 1,
+                };
+                list.push(Item::Number(val, region.clone()));
+                num.clear();
+                region = Region::default();
+            }
+            list
+        })
+        .collect();
+    let mut sum = 0;
+    things.windows(2).for_each(|win| {
+        for lhs in win[0].iter() {
+            for rhs in win[1].iter() {
+                // given lhs is num and rhs is a symbol
+                if let Item::Number(num, reg) = lhs {
+                    if let Item::Symbol(loc) = rhs {
+                        if reg.contains(loc) {
+                            sum += num;
+                        }
+                    }
+                // given rhs is num and lhs is a symbol
+                } else if let Item::Symbol(loc) = lhs {
+                    if let Item::Number(num, reg) = rhs {
+                        if reg.contains(loc) {
+                            sum += num;
+                        }
+                    }
+                }
+            }
+        }
+    });
+    // need the case for checking within its own line
+    for list in things.iter() {
+        if list.len() >= 2 {
+            list.windows(2).for_each(|win| {
+                // if there are symbols and numbers next to each other then check if they apply
+                if let Item::Number(num, reg) = &win[0] {
+                    if let Item::Symbol(loc) = &win[1] {
+                        if reg.contains(&loc) {
+                            sum += num;
+                        }
+                    }
+                // given rhs is num and lhs is a symbol
+                } else if let Item::Symbol(loc) = &win[0] {
+                    if let Item::Number(num, reg) = &win[1] {
+                        if reg.contains(&loc) {
+                            sum += num;
+                        }
+                    }
+                }
+            });
+        }
+    }
+    sum
+}
+
+#[cfg(test)]
+#[path = "../test/test1.rs"]
+mod test1;
---
+++ 2023/day-03/src/test/test2.rs
@@ -0,0 +1,25 @@
+use super::*;
+
+#[test]
+fn case1() {
+    assert_eq!(
+        467835,
+        parse_schematic(String::from(
+            "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598.."
+        ))
+    );
+}
+
+#[test]
+fn case2() {
+    assert_eq!(64, parse_schematic(String::from("2*4*8")));
+}
