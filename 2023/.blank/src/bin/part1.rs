fd078b67c91d9d217bd154f2e70017a629effede
Mick Harrigan <mharrigan328@gmail.com>
2023-12-13 11:09:35 -0800

    Updated the blank dir to have more of the reused code stay in each day

--- 2023/.blank/src/bin/part1.rs
+++ 2023/.blank/src/bin/part1.rs
@@ -1,3 +1,21 @@
+use anyhow::Result;
+use regex::Regex;
+use std::{fs::File, io::Read};
+
+fn main() -> Result<()> {
+    // read in the inputs of the file to the contents string
+    let mut input = File::open(format!("{}/{}", env!("CARGO_MANIFEST_DIR"), "input1.txt"))?;
+    let mut contents = String::new();
+    input.read_to_string(&mut contents)?;
+    let out = parse_things(contents);
+    println!("Output: {}", out);
+    Ok(())
+}
+
+fn parse_things(contents: String) -> usize {
+    0
+}
+
 #[cfg(test)]
 #[path = "../test/test1.rs"]
 mod test1;
